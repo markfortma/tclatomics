423fe1c1b7969ca67329686fc3e3fc3a440d6282
Matthew Markfort <matthew.markfort@gmail.com>
2016-05-09 11:37:05 -0500

    Add tclblocks.py

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # tclblocks
+
+This is a small snippet from a larger project. It
+has been distilled to the bare minimum as a demonstration.
+
+For a license, please add references to this repository
+and author if used in other projects.
+
+Additionally, there is no guarantee with this code. Use
+at your own risk.
+
---
+++ tclblocks.py
@@ -0,0 +1,94 @@
+# tclblocks.py
+#
+# This is a function from a much larger project, and it has been stripped down for demonstration purposes.
+
+__date__ = '2016-05-09'
+
+tclcode = str()
+
+def tclblocks(start=0):
+    """
+    Recursively evaluate tclcode and create a one-dimension list of the Tcl whereby the contents between '{' and '}'
+    are one element and between each Tcl block is also one element
+    :param start: (int) the index of the character we process from
+    :return: (tuple) [of length 2] where element 0 is the one-dimension list and element 1 is the index
+    """
+    global tclcode
+    bracecount = 0
+    buffer = str()
+    blocks = list()
+    ignore = False
+    inquote = 0
+    incommand = 0
+    dex = start
+    sector = len(tclcode)
+    while dex < sector:
+        if tclcode[dex] == '"':
+            inquote += 1
+            buffer += tclcode[dex]
+        elif tclcode[dex] == '[':
+            incommand += 1
+            buffer += tclcode[dex]
+        elif tclcode[dex] == ']':
+            incommand -= 1
+            buffer += tclcode[dex]
+        elif tclcode[dex] == '#':
+            ignore = True
+        elif tclcode[dex] == '\n':
+            ignore = False
+            buffer = buffer.strip()  # Clean off any whitespace fore and aft
+            if len(buffer):
+                blocks.append(buffer)
+            buffer = str()
+        elif tclcode[dex] == ';':
+            if inquote > 0:
+                buffer += tclcode[dex]
+            else:
+                buffer = buffer.strip()  # Clean off any whitespace fore and aft
+                if len(buffer):
+                    blocks.append(buffer)
+                buffer = str()
+        elif tclcode[dex] == '{':
+            bracecount += 1
+            if incommand != 0:
+                buffer += tclcode[dex]
+            elif inquote % 2 != 0:
+                buffer += tclcode[dex]
+            elif bracecount % 2 != 0:
+                buffer = buffer.strip()
+                if len(buffer):
+                    blocks.append(buffer)
+                temp, dex = tclblocks(dex + 1)
+                blocks += temp
+                buffer = str()
+            else:
+                buffer = buffer.strip()
+                if len(buffer):
+                    blocks.append(buffer)
+                buffer = str()
+        elif tclcode[dex] == '}':
+            bracecount -= 1
+            if incommand != 0:
+                buffer += tclcode[dex]
+            elif inquote % 2 != 0:
+                buffer += tclcode[dex]
+            elif bracecount % 2 == 0:
+                buffer = buffer.strip()
+                if len(buffer):
+                    blocks.append(buffer)
+                return blocks, dex
+            else:
+                buffer = buffer.strip()
+                if len(buffer):
+                    blocks.append(buffer)
+                buffer = str()
+        elif not ignore:
+            buffer += tclcode[dex]
+        dex += 1
+    return blocks, dex
+
+if __name__ == '__main__':
+    tclcode = '''when HTTP_REQUEST {
+    HTTP::redirect "https://[HTTP::host][HTTP::uri]"
+}'''
+    print(tclblocks())
